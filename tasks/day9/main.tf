0e2be413adf04e95cd6569983e5aa086994c19bc
manu <gatlamanohar199@gmail.com>
2023-04-08 15:00:34 +0530

    added changes

--- tasks/day9/main.tf
+++ tasks/day9/main.tf
@@ -9,7 +9,7 @@ module "aws_ec2" {
     lb_subnet_names      = ["web", "app"]
     lb_subnets_names_azs = ["a", "b"]
     lb_vpc_cidr          = "192.168.0.0/16"
-    rollout_versions     = "0.0.0.0"
+    rollout_versions     = "0.0.0.1"
   }
 }
 output "redurl" {
